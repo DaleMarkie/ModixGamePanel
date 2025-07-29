700689265da9cd58dc045ad7cabec27106fb2638
ov3rlord1 <dalemarkie03@gmail.com>
2025-07-29 11:30:43 +0000

    SRC - testpage.

--- src/app/testpage copy/page.tsx
+++ src/app/testpage copy/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React from "react";
-import DashboardLayout from "@components/sidebar/DashboardLayout";
-import Terms from "./Terms";
 
-export default function Terms() {
+import DashboardLayout from "@components/sidebar/DashboardLayout";
+import Testpage from "./Terms"; // <-- Correct import
+
+export default function TermsPage() {
   return (
     <DashboardLayout>
-      <Terms />
+      <Testpage />
     </DashboardLayout>
   );
 }
