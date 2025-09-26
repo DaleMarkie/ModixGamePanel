cbdc32f862fdfb3ee2f17308e3a7f656d3518a70
ov3rlord <modixgamepanel@gmail.com>
2025-09-26 10:12:45 +0000

    [build changes]

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const { spawn } = require("child_process");
 const isDev = require("electron-is-dev");
 
 let mainWindow;
+let backendProcess;
+
+function startBackend() {
+  // Cross-platform Node backend start
+  const backendScript = path.join(__dirname, "scripts", "startBackend.js");
+
+  backendProcess = spawn(process.execPath, [backendScript], {
+    cwd: __dirname,
+    shell: true,
+    stdio: "inherit",
+  });
+
+  backendProcess.on("close", (code) => {
+    console.log(`Backend exited with code ${code}`);
+  });
+}
+
+function getFrontendURL() {
+  if (isDev) {
+    // Dev server
+    return "http://localhost:3000";
+  } else {
+    // Production build
+    // Next.js .next server pages path
+    // Works on all OSes
+    return `file://${path.join(
+      __dirname,
+      "frontend",
+      ".next",
+      "server",
+      "pages",
+      "index.html"
+    )}`;
+  }
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -15,26 +51,24 @@ function createWindow() {
     },
   });
 
-  if (isDev) {
-    // In dev: load Next.js server
-    mainWindow.loadURL("http://localhost:3000");
-  } else {
-    // In prod: load Next.js build
-    mainWindow.loadFile(path.join(__dirname, "frontend", "out", "index.html"));
-    // or path.join(__dirname, "build", "index.html") if using CRA
-  }
+  mainWindow.loadURL(getFrontendURL());
 
   mainWindow.on("closed", () => {
     mainWindow = null;
+    if (backendProcess) backendProcess.kill();
+    app.quit();
   });
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  startBackend();
+  createWindow();
+});
 
+// Quit when all windows are closed
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  if (backendProcess) backendProcess.kill();
+  if (process.platform !== "darwin") app.quit();
 });
 
 app.on("activate", () => {
